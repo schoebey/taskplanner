e9ecc805d37438b28581ce0d5b960bd54a1b10f3
rschaub <rschaub@ancor.ch>
2019-03-06 20:52:21 +0100

    some more work on link widget tooltips

--- gui/linkwidget.h
+++ gui/linkwidget.h
@@ -1,12 +1,36 @@
 #ifndef LINKWIDGET_H
 #define LINKWIDGET_H
 
+#include <QStyleOption>
 #include <QFrame>
 #include <QUrl>
 
 namespace Ui {
   class LinkWidget;
 }
+
+enum CustomStyleOptions
+{
+  eLinkWidget = QStyleOption::SO_CustomBase + 1
+};
+
+enum CustomControlElements
+{
+  CE_LinkWidget = QStyle::CE_CustomBase + 1
+};
+
+class QStyleOptionLinkWidget : public QStyleOption
+{
+public:
+  QStyleOptionLinkWidget();
+
+  QRect labelRect;
+  QRect iconRect;
+  bool bDrawFrame = false;
+  double dPaddingX = 0;
+  double dPaddingY = 0;
+  double dBorderRadius = 0;
+};
 
 class LinkWidget : public QFrame
 {
@@ -19,7 +43,13 @@ public:
   void setConstrainLabelToSize(bool);
   bool constrainLabelToSize() const;
 
-  void showToolTip();
+  Q_PROPERTY(bool drawFrame READ drawFrame WRITE setDrawFrame)
+  void setDrawFrame(bool);
+  bool drawFrame() const;
+
+  Q_PROPERTY(double borderRadius READ borderRadius WRITE setBorderRadius)
+  void setBorderRadius(double dRadius);
+  double borderRadius() const;
 
 private:
   void enterEvent(QEvent* pEvent) override;
@@ -30,6 +60,8 @@ protected:
   Ui::LinkWidget* ui;
   QUrl m_link;
   bool m_bConstrainLabelToSize = false;
+  bool m_bDrawFrame = false;
+  double m_dBorderRadius = 0;
   LinkWidget* m_pToolTip = nullptr;
 };
 
