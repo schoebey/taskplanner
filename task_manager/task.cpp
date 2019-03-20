9993a65b94d94cbd52c7b690d0b77f469f95a4ab
rschaub <rschaub@ancor.ch>
2019-03-20 21:39:22 +0100

    user-defined priority is now weighed when computing autoPriorities

--- task_manager/task.cpp
+++ task_manager/task.cpp
@@ -187,7 +187,18 @@ double Task::autoPriority() const
 
   // TODO: incorporate factor (hi/med/lo)
   // TODO: replace with std::tie
-  return dDueTimeWeight +
+  int iUserDefinedPriority = 0;
+  if (hasPropertyValue("priority"))
+  {
+    iUserDefinedPriority = property<int>("priority");
+
+    // if a user defined priority is set, scale down the due date related priority
+    // such that two tasks with the same user defined priority get weighed against
+    // each other according to their due date.
+    dDueTimeWeight /= 10;
+  }
+
+  return dDueTimeWeight + iUserDefinedPriority +
       static_cast<double>(iNofDaysInSystem) / 100.;
 }
 
