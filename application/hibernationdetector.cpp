cae5a4f594d18d3ecdb0c9c74b43cf81e723bc56
rschaub <rschaub@ancor.ch>
2019-07-12 17:56:33 +0200

    hibernation detection can now be en/disabled via menu

--- application/hibernationdetector.cpp
+++ application/hibernationdetector.cpp
@@ -3,7 +3,8 @@
 HibernationDetector::HibernationDetector(QObject *parent)
   : QObject(parent),
     m_previousSample(QDateTime::currentDateTime()),
-    m_iDetectionDeltaTSeconds(300)
+    m_iDetectionDeltaTSeconds(300),
+    m_bEnabled(true)
 {
   startTimer(30000);
 }
@@ -18,15 +19,23 @@ qint64 HibernationDetector::minDetectionDeltaT() const
   return m_iDetectionDeltaTSeconds;
 }
 
-void HibernationDetector::timerEvent(QTimerEvent* pEvent)
+void HibernationDetector::setEnabled(bool bEnabled)
+{
+  m_bEnabled = bEnabled;
+}
+
+void HibernationDetector::timerEvent(QTimerEvent* /*pEvent*/)
 {
   QDateTime now = QDateTime::currentDateTime();
 
-  auto deltaSeconds = m_previousSample.secsTo(now);
-
-  if (deltaSeconds > m_iDetectionDeltaTSeconds)
+  if (m_bEnabled)
   {
-    emit wokeUpFromHibernation(m_previousSample, now);
+    auto deltaSeconds = m_previousSample.secsTo(now);
+
+    if (deltaSeconds > m_iDetectionDeltaTSeconds)
+    {
+      emit wokeUpFromHibernation(m_previousSample, now);
+    }
   }
 
   m_previousSample = now;
