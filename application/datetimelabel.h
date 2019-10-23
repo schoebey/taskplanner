068fb7d5c338f846626ba3e8b5852f2e73ccbe8e
rschaub <rschaub@ancor.ch>
2019-10-23 08:12:23 +0200

    date time editor now has a popup to select the date

--- application/datetimelabel.h
+++ application/datetimelabel.h
@@ -5,6 +5,7 @@
 
 #include <QTimer>
 
+class QCalendarWidget;
 class DateTimeLabel : public EditableLabel
 {
   Q_OBJECT
@@ -13,14 +14,23 @@ public:
 
   DateTimeLabel(QWidget* pParent);
 
+  void edit() override;
+
 signals:
   void attentionNeeded();
 
 private:
   void onEditingFinishedImpl() override;
+  bool eventFilter(QObject* pObj, QEvent* pEvent);
+  void rejectAndCloseEditor();
+  void onTextChanged(const QString& sText);
 
 private:
   QTimer m_updateTimer;
+  QWidget* m_pEditor;
+  QLineEdit* m_pDateTimeLineEdit;
+  QCalendarWidget* m_pCalendarWidget;
+
 };
 
 #endif // DATETIMELABEL_H
