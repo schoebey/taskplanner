536809e5447568fed0f3550447c5731d9fc2a8f9
rschaub <rschaub@ancor.ch>
2020-03-02 08:14:49 +0100

    test template implementation for draggable

--- application/tagwidget.cpp
+++ application/tagwidget.cpp
@@ -1,12 +1,17 @@
 #include "tagwidget.h"
 #include "styleExtension.h"
 
+#include <QMouseEvent>
 #include <QPainter>
 #include <QStyleOption>
+#include <QApplication>
+
+#include <cmath>
 
 
 TagWidget::TagWidget(const QString& sText, QWidget* pParent)
-  : QLabel(sText, pParent)
+  : QLabel(sText, pParent),
+    Draggable<TagWidget> (this)
 {
 
 }
@@ -17,5 +22,22 @@ void TagWidget::paintEvent(QPaintEvent* /*pEvent*/)
   opt.initFrom(this);
 
   QPainter painter(this);
-  style()->drawControl(static_cast<QStyle::ControlElement>(CE_TagWidget), &opt, &painter, this);
+  style()->drawControl(customControlElements::CE_TagWidget, &opt, &painter, this);
+}
+
+void TagWidget::mouseMoveEvent(QMouseEvent* pMouseEvent)
+{
+  QFrame::mouseMoveEvent(pMouseEvent);
+
+  onMouseMove(pMouseEvent);
+}
+
+void TagWidget::mousePressEvent(QMouseEvent* pMouseEvent)
+{
+  setMouseDown(true, pMouseEvent->pos());
+}
+
+void TagWidget::mouseReleaseEvent(QMouseEvent* /*pMouseEvent*/)
+{
+  setMouseDown(false);
 }
--- application/tagwidget.h
+++ application/tagwidget.h
@@ -2,13 +2,127 @@
 #define TAGWIDGET_H
 
 #include <QLabel>
+#include <QMouseEvent>
+#include <QApplication>
 
-class TagWidget : public QLabel
+#include <cmath>
+
+template<typename T>
+class DraggableContainer
+{
+public:
+  DraggableContainer()
+  {}
+
+  ~DraggableContainer()
+  {}
+
+  bool addItem(T* pT)
+  {
+    if (nullptr == pT) { return false; }
+    return addItem_impl(pT);
+  }
+
+  bool removeItem(T* pT)
+  {
+    if (nullptr == pT) { return false; }
+    return removeItem_impl(pT);
+  }
+
+private:
+  virtual bool addItem_impl(T* pT) = 0;
+  virtual bool removeItem_impl(T* pT) = 0;
+};
+
+class A : public DraggableContainer<int>
+{
+public:
+  A() {}
+  ~A() {}
+
+  bool addItem_impl(int* pInt) override { return true; }
+  bool removeItem_impl(int* pInt) override { return true; }
+};
+
+template<typename T>
+class Draggable
+{
+public:
+  Draggable(T* pThis)
+    : m_pThis(pThis)
+  {}
+
+  ~Draggable()
+  {}
+
+  void setMouseDown(bool bMouseDown, QPoint pt = QPoint())
+  {
+    m_bMouseDown = bMouseDown;
+    m_mouseDownPoint = pt;
+  }
+
+  bool mouseDown() const
+  {
+    return m_bMouseDown;
+  }
+
+  QPoint mouseDownPoint() const
+  {
+    return m_mouseDownPoint;
+  }
+
+  void setDraggingInstance(T* pT)
+  {
+    m_pDraggingInstance = pT;
+  }
+
+  T* draggingInstance() const
+  {
+    return m_pDraggingInstance;
+  }
+
+  void onMouseMove(QMouseEvent* pMouseEvent)
+  {
+    if (mouseDown() && nullptr == draggingInstance())
+    {
+      QPoint ptDist = pMouseEvent->pos() - mouseDownPoint();
+      if (20 < std::sqrt(ptDist.x() * ptDist.x() + ptDist.y() * ptDist.y()))
+      {
+        setDraggingInstance(m_pThis);
+
+        if (nullptr != m_pContainer)
+        {
+          m_pContainer->removeItem(m_pThis);
+        }
+
+        qApp->installEventFilter(m_pThis);
+        m_pThis->setParent(m_pThis->window());
+        m_pThis->setFocus();
+        m_pThis->move(pMouseEvent->globalPos() - mouseDownPoint());
+        m_pThis->show();
+        m_pThis->raise();
+        pMouseEvent->accept();
+      }
+    }
+  }
+
+private:
+  bool m_bMouseDown = false;
+  QPoint m_mouseDownPoint;
+  T* m_pThis = nullptr;
+  T* m_pDraggingInstance = nullptr;
+  DraggableContainer<T>* m_pContainer = nullptr;
+};
+
+class TagWidget : public QLabel, public Draggable<TagWidget>
 {
 public:
   TagWidget(const QString& sText, QWidget* pParent);
 private:
   void paintEvent(QPaintEvent* pEvent) override;
+  void mouseMoveEvent(QMouseEvent* pMouseEvent) override;
+  void mousePressEvent(QMouseEvent* pMouseEvent) override;
+  void mouseReleaseEvent(QMouseEvent* pMouseEvent) override;
 };
 
 #endif // TAGWIDGET_H
