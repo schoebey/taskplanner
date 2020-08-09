7140dd8e228b945373531cd1f9715cd8acbd9a19
ballessay <code@ballessay.de>
2020-08-09 20:47:59 +0200

    Only run nproc jobs

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -8,8 +8,12 @@ source $QT_ENV_SCRIPT
 
 cmake -G"Unix Makefiles" -DCMAKE_INSTALL_PREFIX=/usr ..
 
-cmake --build . --target all -- -j
+cmake --build . --target all -- -j$(nproc)
 
 DESTDIR=install cmake --build . --target install
 
-#TODO: linuxdeployqt
+wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
+chmod a+x linuxdeployqt-continuous-x86_64.AppImage
+export LD_LIBRARY_PATH=$(pwd)/install/usr/lib
+# export VERSION=...
+./linuxdeployqt-continuous-x86_64.AppImage install/usr/share/applications/*.desktop -appimage
