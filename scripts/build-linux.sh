338c7aeb5f71376b89b6a86dd8ef8ed20fcd403c
ballessay <code@ballessay.de>
2020-08-10 19:09:25 +0200

    build: Add env output

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -3,6 +3,8 @@
 mkdir build
 cd build
 
+env
+
 QT_ENV_SCRIPT=$(find /opt -name 'qt*-env.sh')
 source $QT_ENV_SCRIPT
 
@@ -12,8 +14,10 @@ cmake --build . --target all -- -j$(nproc)
 
 DESTDIR=install cmake --build . --target install
 
+env
+
 wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod a+x linuxdeployqt-continuous-x86_64.AppImage
 export LD_LIBRARY_PATH=$(pwd)/install/usr/lib
-# export VERSION=...
+export VERSION="v0.8.0"
 ./linuxdeployqt-continuous-x86_64.AppImage install/usr/share/applications/*.desktop -appimage
