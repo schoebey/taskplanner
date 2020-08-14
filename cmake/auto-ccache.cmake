3c7c259b24116faffb9901f963590d806553a185
ballessay <code@ballessay.de>
2020-08-14 19:36:07 +0200

    Fix auto-cache

--- cmake/auto-ccache.cmake
+++ cmake/auto-ccache.cmake
@@ -1,9 +1,9 @@
 # Auto. CCACHE
-#if(UNIX)
-#    find_program(CCACHE_FOUND ccache)
+if(UNIX)
+    find_program(CCACHE_FOUND ccache)
 
-#    if(CCACHE_FOUND)
-#        set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ccache)
-#        set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK ccache)
-#    endif(CCACHE_FOUND)
-#endif()
+    if(CCACHE_FOUND)
+        set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ccache)
+        set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK ccache)
+    endif(CCACHE_FOUND)
+endif()
