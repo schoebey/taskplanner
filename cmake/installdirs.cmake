2caa4080cbc6814f94f68d68a81131ca46d8c986
ballessay <code@ballessay.de>
2020-08-14 19:11:05 +0200

    First macOS app

--- cmake/installdirs.cmake
+++ cmake/installdirs.cmake
@@ -1,14 +1,32 @@
-set(RUNTIME_INSTALL_DIR bin
-    CACHE PATH "Install dir for executables and dlls")
+if(UNIX AND NOT APPLE)
+    message("UNIX NOT APPLE")
+    set(RUNTIME_INSTALL_DIR bin
+        CACHE PATH "Install dir for executables and dlls")
 
-set(ARCHIVE_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/lib
-    CACHE PATH "Install dir for static libraries")
+    set(ARCHIVE_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/lib
+        CACHE PATH "Install dir for static libraries")
 
-set(LIBRARY_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/lib
-    CACHE PATH "Install dir for shared libraries")
+    set(LIBRARY_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/lib
+        CACHE PATH "Install dir for shared libraries")
 
-set(PLUGIN_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/lib/${APPLICATION_NAME}
-    CACHE PATH "Install dir for plugins")
+    set(PLUGIN_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/lib/${APPLICATION_NAME}
+        CACHE PATH "Install dir for plugins")
+elseif(APPLE)
+    message("APPLE")
+    set(prefix "${PROJECT_NAME}.app/Contents")
+
+    set(RUNTIME_INSTALL_DIR ${prefix}/MacOS
+        CACHE PATH "Install dir for executables and dlls")
+
+    set(ARCHIVE_INSTALL_DIR ${prefix}/Frameworks
+        CACHE PATH "Install dir for static libraries")
+
+    set(LIBRARY_INSTALL_DIR ${prefix}/Frameworks
+        CACHE PATH "Install dir for shared libraries")
+
+    set(PLUGIN_INSTALL_DIR ${prefix}/PlugIns
+        CACHE PATH "Install dir for plugins")
+endif()
 
 mark_as_advanced(RUNTIME_INSTALL_DIR ARCHIVE_INSTALL_DIR  LIBRARY_INSTALL_DIR PLUGIN_INSTALL_DIR)
 
