29e424f92ff4f49d20f007a35bdfef99d8bd4831
rschaub <rschaub@ancor.ch>
2020-09-11 19:06:29 +0200

    registered properties 'tags' and 'tasks'

--- application/main.cpp
+++ application/main.cpp
@@ -82,6 +82,8 @@ int main(int argc, char *argv[])
   REGISTER_PROPERTY(Task, "sort_priority", int, false);
   REGISTER_PROPERTY(Task, "priority", int, true);
   REGISTER_PROPERTY(Task, "color", QColor, false);
+  REGISTER_PROPERTY(Task, "tasks", std::vector<task_id>, false);
+  REGISTER_PROPERTY(Task, "tags", std::vector<tag_id>, false);
   Properties<Task>::registerConstraint("category", ONE_OF(QString("a"), QString("b"), QString("c")));
   Properties<Task>::registerConstraint("duration (days)", MIN(0));
 
