f1e4373c2cf202445f6f304f30dd09997622fb90
rschaub <rschaub@ancor.ch>
2020-09-15 08:00:18 +0200

    new class TagEditor that lets user add, delete and modify tags

--- application/tageditor.cpp
+++ application/tageditor.cpp
@@ -1,6 +1,28 @@
 #include "tageditor.h"
+#include "tagproviderinterface.h"
+#include "tagwidget.h"
 
-TagEditor::TagEditor()
+#include <QGridLayout>
+
+TagEditor::TagEditor(ITagProvider* pTagProvider)
+  : m_pTagProvider(pTagProvider)
 {
+  setupUi();
+}
 
+void TagEditor::setupUi()
+{
+  QGridLayout* pLayout = new QGridLayout(this);
+  setLayout(pLayout);
+
+  if (nullptr != m_pTagProvider)
+  {
+    int iRow = 0;
+    for (const STagData& el : m_pTagProvider->availableTags())
+    {
+      TagWidget* pTagWidget = new TagWidget(el.id, el.sName, this);
+      pTagWidget->setColor(el.color);
+      pLayout->addWidget(pTagWidget, iRow++, 0);
+    }
+  }
 }
--- application/tageditor.h
+++ application/tageditor.h
@@ -1,11 +1,26 @@
 #ifndef TAGEDITOR_H
 #define TAGEDITOR_H
 
+#include "id_types.h"
+#include <QWidget>
 
-class TagEditor
+class ITagProvider;
+class TagEditor : public QWidget
 {
 public:
-  TagEditor();
+  TagEditor(ITagProvider* pTagProvider);
+
+signals:
+  void tagDeleted(tag_id tagId);
+  void tagEdited(tag_id tagId, const QString& sText, const QColor& color);
+
+private:
+  void setupUi();
+  void addTag();
+  void deleteTag(tag_id tagId);
+  void editTag(tag_id tagId, const QString& sText, const QColor& color);
+
+  ITagProvider* m_pTagProvider = nullptr;
 };
 
 #endif // TAGEDITOR_H
