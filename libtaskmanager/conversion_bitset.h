8575cbbf87286f2b0f297fdeac515a4d0ec0e5be
rschaub <rschaub@ancor.ch>
2020-10-12 08:11:02 +0200

    project file and new conversion for std::bitset

--- libtaskmanager/conversion_bitset.h
+++ libtaskmanager/conversion_bitset.h
@@ -1,4 +1,33 @@
 #ifndef CONVERSION_BITSET_H
 #define CONVERSION_BITSET_H
 
+#include <QString>
+#include <bitset>
+
+namespace detail {
+  template<int count>
+  struct to_string<std::bitset<count>>
+  {
+    static QString convert(const std::bitset<count>& bs)
+    {
+      return QString::fromStdString(bs.to_string());
+    }
+  };
+
+  template<int count>
+  struct from_string<std::bitset<count>> {
+    static std::bitset<count> convert(const QString& s, bool& bConversionStatus)
+    {
+      try {
+        bConversionStatus = true;
+        return std::bitset<count>(s.toStdString());
+      }
+      catch (std::invalid_argument) {
+        bConversionStatus = false;
+      }
+      return std::bitset<count>{};
+    }
+  };
+}
+
 #endif // CONVERSION_BITSET_H
