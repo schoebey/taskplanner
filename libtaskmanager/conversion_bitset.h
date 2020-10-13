d089aa03f463db1ea2b9eedd3e56fe84a7c03d02
rschaub <rschaub@ancor.ch>
2020-10-13 17:49:28 +0200

    moved displayString-method of bitset<7> to bitset-header

--- libtaskmanager/conversion_bitset.h
+++ libtaskmanager/conversion_bitset.h
@@ -30,4 +30,22 @@ namespace detail {
   };
 }
 
+namespace conversion {
+  QString bitsetToWeekDays(const std::bitset<7>& bs)
+  {
+    if (0b1100000 == bs.to_ulong()) {
+      return "every weekend";
+    }
+    else if (0b0011111 == bs.to_ulong()) {
+      return "every workday";
+    }
+
+    QStringList l;
+    for (int i = 0; i < 7; ++i)
+    {
+      if (bs[i]) { l.push_back(QDate::shortDayName(i+1)); }
+    }
+    return QString("every %1").arg(l.join(", "));
+  }
+}
 #endif // CONVERSION_BITSET_H
