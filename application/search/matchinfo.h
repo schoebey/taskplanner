d839a47683457be85e2662977455788be2ce4819
rschaub <rschaub@ancor.ch>
2021-01-17 15:00:33 +0100

    bugfix for search
    due to the possibly delayed creation of task widgets, search
    iteration had to be adapted to expand task widgets on demand

--- application/search/matchinfo.h
+++ application/search/matchinfo.h
@@ -1,18 +1,24 @@
 #ifndef MATCHINFO_H
 #define MATCHINFO_H
 
+#include "id_types.h"
+
 #include <QMetaType>
 #include <QRect>
 #include <QPointer>
 
 #include <vector>
+#include <deque>
 
+class TaskWidget;
 struct SMatchInfo
 {
   QPointer<QWidget> pWidget;
   QRect rect;
   int iStart = -1;
   int iSize = -1;
+  QPointer<TaskWidget> pClosestParentTaskWidget;
+  std::deque<task_id> vToExpand;
 };
 typedef std::vector<SMatchInfo> tvMatchInfo;
 Q_DECLARE_METATYPE(SMatchInfo)
