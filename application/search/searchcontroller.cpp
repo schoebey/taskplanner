9953c553686409b7885605066224aa10f1293c54
rschaub <rschaub@ancor.ch>
2021-01-17 15:00:33 +0100

    bugfix for search
    due to the possibly delayed creation of task widgets, search
    iteration had to be adapted to expand task widgets on demand

--- application/search/searchcontroller.cpp
+++ application/search/searchcontroller.cpp
@@ -3,7 +3,9 @@
 #include "widgetmanager.h"
 #include "groupwidget.h"
 #include "taskwidget.h"
+#include "tasklistwidget.h"
 #include "taskinterface.h"
+#include "groupinterface.h"
 
 
 #include <QRegularExpression>
@@ -177,16 +179,16 @@ namespace {
   {
 
     Qt::CaseSensitivity cs = bCaseSensitive ? Qt::CaseSensitive : Qt::CaseInsensitive;
-    pTask->name();
-    pTask->description();
+    QString sName = pTask->name();
+    QString sDesc = pTask->description();
 
     std::function<detail::SSearchResult(const QString&, const QString&, Qt::CaseSensitivity, int)> fnFind
-        = bRegExp ? detail::findStr : detail::findRx;
+        = bRegExp ? detail::findRx : detail::findStr;
 
     detail::SSearchResult sr;
-    sr = fnFind(pTask->name(), sTerm, cs, -1);
+    sr = fnFind(sTerm, pTask->name(), cs, -1);
     if (sr.iIndex != -1)  { return true; }
-    sr = fnFind(pTask->description(), sTerm, cs, -1);
+    sr = fnFind(sTerm, pTask->description(), cs, -1);
     if (sr.iIndex != -1)  { return true; }
 
     return false;
@@ -236,11 +238,40 @@ void SearchController::onSearchTermChanged(const QString& sTerm)
   }
 
   m_hits.clear();
+
+
+  /* whenever the search term changes, we iterate through all existing
+   * tasks and check if they contain a match (non-recursively).
+   *
+   * for all matches, it is then determined if the corresponding task widget
+   * exists. If not (if the parent hasn't been expanded yet), the hit
+   * has to be stored along with information on how to reach the widget.
+   *
+   * Then, in a second sweep, we iterate through all existing top level tasks
+   * of a group widget and call 'find' which recursively tries to match
+   * the term with all its contained widgets.
+   *
+   * This will build the initial hit list. Together with the first sweep,
+   * the hit list now contains both fully accessible matches as well as
+   * matches on widgets that yet need to be reconstructed.
+   *
+   * Since the algorithm tries to optimize for speed, the reconstruction
+   * process uses lazy evaluation, meaning a widget is reconstructed
+   * only when the hit iterator is moved to its position in the hit list.
+   */
+
+
   if (!sTerm.isEmpty())
   {
-    for (const auto& taskId : m_pTaskManager->taskIds())
-    {
+    std::map<group_id, tvMatchInfo> matchesByGroup;
+
+    for (const auto& taskId : m_pTaskManager->taskIds()) {
+
       ITask* pTask = m_pTaskManager->task(taskId);
+
+      group_id groupId = pTask->group();
+      tvMatchInfo& vMatchInfo = matchesByGroup[groupId];
+
       if (containsMatch(sTerm, pTask, m_options.bCaseSensitive, m_options.bRegularExpression))
       {
         // ensure the corresponding widget exists by expanding all of the parent widgets
@@ -254,26 +285,27 @@ void SearchController::onSearchTermChanged(const QString& sTerm)
           pTask = m_pTaskManager->task(id);
           if (-1 == id) { break; }
           pTaskWidget = m_pWidgetManager->taskWidget(id);
+          pTask = m_pTaskManager->task(id);
         }
 
-        if (nullptr != pTaskWidget)
-        {
-          // consecutively expand the widget until the target widget becomes accessible
-          detail::ensureVisible(pTaskWidget);
-
-          while (!vWidgetsToReconstruct.empty())
-          {
-            task_id childId = vWidgetsToReconstruct.front();
-            vWidgetsToReconstruct.pop_front();
-            TaskWidget* pChildWidget = m_pWidgetManager->taskWidget(childId);
-            detail::ensureVisible(pChildWidget);
-          }
+        // instead of expanding the tasks right away, store
+        // the ids in the results container
+        // This way, a delayed expansion can take place
+        // when the iterator hits an unexpanded result.
+        if (!vWidgetsToReconstruct.empty()) {
+          SMatchInfo hit;
+          hit.pClosestParentTaskWidget = pTaskWidget;
+          hit.vToExpand = vWidgetsToReconstruct;
+          vMatchInfo.push_back(hit);
         }
       }
     }
 
+
     for (const auto& groupId : m_pTaskManager->groupIds())
     {
+      tvMatchInfo& vMatchInfo = matchesByGroup[groupId];
+
       auto pGroupWidget = m_pWidgetManager->groupWidget(groupId);
       if (nullptr != pGroupWidget)
       {
@@ -281,10 +313,26 @@ void SearchController::onSearchTermChanged(const QString& sTerm)
         {
           if (nullptr != pTask)
           {
-            find(sTerm, pTask, m_options.bCaseSensitive, m_options.bRegularExpression, m_hits);
+            find(sTerm, pTask, m_options.bCaseSensitive, m_options.bRegularExpression, vMatchInfo);
           }
         }
       }
+
+      // sort all detected matches according to their y position
+      std::sort(vMatchInfo.begin(), vMatchInfo.end(),
+                [pGroupWidget](const SMatchInfo& lhs, const SMatchInfo& rhs)
+      {
+        QWidget* pLhs = nullptr != lhs.pWidget ? lhs.pWidget : lhs.pClosestParentTaskWidget->taskListWidget();
+        QWidget* pRhs = nullptr != rhs.pWidget ? rhs.pWidget : rhs.pClosestParentTaskWidget->taskListWidget();
+
+        if (nullptr != pLhs && nullptr != pRhs){
+          return pLhs->mapTo(pGroupWidget, pLhs->pos()).y() < pRhs->mapTo(pGroupWidget, pRhs->pos()).y();
+        }
+
+        return false;
+      });
+
+      m_hits.insert(m_hits.end(), vMatchInfo.begin(), vMatchInfo.end());
     }
   }
 
@@ -301,20 +349,16 @@ void SearchController::onSearchTermChanged(const QString& sTerm)
   }
 
   m_hitIter = m_hits.end();
-  auto it = std::find_if(m_hits.begin(), m_hits.end(),
-                         [pCurrentWidget, iCurrentIndex](const tMatches::value_type& el )
-  {
-    return el.pWidget == pCurrentWidget && el.iStart == iCurrentIndex;
-  });
+  tMatches::iterator it = m_hits.begin();
+  if (nullptr != pCurrentWidget && -1 != iCurrentIndex) {
+    it = std::find_if(m_hits.begin(), m_hits.end(),
+                           [pCurrentWidget, iCurrentIndex](const tMatches::value_type& el )
+    {
+      return el.pWidget == pCurrentWidget && el.iStart == iCurrentIndex;
+    });
+  }
 
-  if (m_hits.end() != it)
-  {
-    setCurrent(it);
-  }
-  else
-  {
-    setCurrent(m_hits.begin());
-  }
+  setCurrent(it);
 }
 
 void SearchController::setCurrent(tMatches::iterator it)
@@ -324,6 +368,27 @@ void SearchController::setCurrent(tMatches::iterator it)
     // clear the active highlight
     detail::highlight(m_hitIter->pWidget, tvMatchInfo{}, eActiveSearchResult);
   }
+
+  // determine if the new position falls on a not yet expanded widget
+  if (it != m_hits.end() &&
+      !it->vToExpand.empty()) {
+    detail::ensureVisible(it->pClosestParentTaskWidget);
+
+    while (!it->vToExpand.empty())
+    {
+      task_id childId = it->vToExpand.front();
+      it->vToExpand.pop_front();
+      TaskWidget* pChildWidget = m_pWidgetManager->taskWidget(childId);
+      detail::ensureVisible(pChildWidget);
+    }
+
+    // this call invalidates 'it', so we try to save its position in the vector
+    // and reconstruct it afterwards
+    int iPos = it - m_hits.begin();
+    onSearchTermChanged(m_sSearchTerm);
+    it = m_hits.begin() + iPos;
+  }
+
 
   m_hitIter = it;
   if (m_hitIter != m_hits.end())
@@ -352,12 +417,12 @@ void SearchController::onNext()
     it = m_hits.begin();
   }
 
-  // advance the iterator from this position to the next valid one,
-  // removing all invalid elements inbetween.
-  while (nullptr == it->pWidget)
-  {
-    it = m_hits.erase(it);
-  }
+//  // advance the iterator from this position to the next valid one,
+//  // removing all invalid elements inbetween.
+//  while (nullptr == it->pWidget)
+//  {
+//    it = m_hits.erase(it);
+//  }
 
   setCurrent(it);
 }
